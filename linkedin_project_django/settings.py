5d74bec55014b60820ab6d523786e8c4982d4835
Ozksmnburhan <burhanozksmn@gmail.com>
2024-09-24 17:01:38 +0300

    last

--- linkedin_project_django/settings.py
+++ linkedin_project_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-gy@acqzce&ff6#2a=tbis%)mxw3oyniq%g*y-p*j83fbo#(0(&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
